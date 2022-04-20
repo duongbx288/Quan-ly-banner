01ac52bbec66d403e1317b2aed175968a6da640f
duongbx288 <duongbx183901@gmail.com>
2022-04-20 16:03:37 +0700

    fsdf

--- backend/src/main/java/com/banner_management/backend/repository/BannerRepositoty.java
+++
@@ -1,8 +0,0 @@
-package com.banner_management.backend.repository;
-
-import com.banner_management.backend.entity.BannerEntity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BannerRepositoty extends JpaRepository<BannerEntity, Integer> {
-}
