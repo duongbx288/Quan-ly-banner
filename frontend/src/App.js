1f58ebbe42fe8b6a215f01f7a2b441c063f6598a
Le Ngoc Kien <lengockien2000@gmail.com>
2022-04-23 23:23:09 +0700

    update app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,3 @@
-// import './App.css';
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import BannerManage from "./pages/BannerManage";
-// import Sidebar from "./components/dashboard/Sidebar";
-// import styled from "styled-components";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-// import { AboutUs, OurAim, OurVision } from "./pages/AboutUs";
-// import { Services } from "./pages/Services";
-// import { Events, EventsOne, EventsTwo } from "./pages/Events";
-// import Contact from "./pages/ContactUs";
-// import Support from "./pages/Support";
-// import CreateBanner from "./components/banner/CreateBanner";
-// import UpdateBanner from "./components/banner/UpdateBanner";
-// import DisplayBanner from "./components/section/DisplayBanner";
-// import Layout from './components/dashboard/Layout';
-// import { CheckboxProvider } from './context/CheckboxContext';
-// import { CheckboxArrProvider } from './context/CheckboxListContext'
-// function App({ userInfo }) {
-//   return (
-//     <CheckboxProvider>
-//     <CheckboxArrProvider>
-//     <Router>
-//       <Home>
-//         <Layout>
-//           <Switch>
-//             <Route path="/about-us" exact component={AboutUs} />
-//             <Route path="/banner/manage" exact component={BannerManage} />
-//             <Route path="/banner/create" exact component={CreateBanner} />
-//             <Route path="/banner/update/:code" exact component={UpdateBanner} />
-//             <Route path="/banner/update" exact component={UpdateBanner} />
-//             <Route path="/banner/delete" exact component={DisplayBanner} />
-//             <Route path="/contact" exact component={Contact} />
-//           </Switch>
-//         </Layout>
-//       </Home>
-//     </Router>
-//     </CheckboxArrProvider>
-//     </CheckboxProvider>
-//   );
-// }
-// export const Home = styled.div`
-//   display: flex;
-// `;
-
-// export default App;
 import React, { Component } from 'react';
 import { Switch, Route, Link } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -107,7 +61,7 @@ class App extends Component {
     const { currentUser, showAdminBoard } = this.state;
     return (
       <div className='wrapper'>
-        {currentUser ? (
+         {currentUser ? (
           <Layout logOut={this.logOut} roles={this.roles}>
             <Switch>
               <Route exact path={["/", "/home"]} component={Home} />
@@ -126,9 +80,105 @@ class App extends Component {
           <Route exact path={["/", "/login"]} component={Login} />
         )}
 
+        {/* <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <div className="navbar-nav mr-auto">
+            {showAdminBoard && (
+              <li className="nav-item">
+                <Link to={"/admin"} className="nav-link">
+                  Admin Board
+                </Link>
+              </li>
+            )}
+            {currentUser && (
+              <li className="nav-item">
+                <Link to={"/user"} className="nav-link">
+                  User
+                </Link>
+              </li>
+            )}
+          </div>
+
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.roles}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={this.logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+
+        <Switch>
+          <Route exact path={["/", "/home"]} component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/profile" component={Profile} />
+          <Route path="/user" component={BoardUser} />
+          <Route path="/admin" component={BoardAdmin} />
+        </Switch>  */}
       </div>
     );
   }
 }
 
 export default App;
+
+// import './App.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import BannerManage from "./pages/BannerManage";
+// import Sidebar from "./components/dashboard/Sidebar";
+// import styled from "styled-components";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// import { AboutUs, OurAim, OurVision } from "./pages/AboutUs";
+// import { Services } from "./pages/Services";
+// import { Events, EventsOne, EventsTwo } from "./pages/Events";
+// import Contact from "./pages/ContactUs";
+// import Support from "./pages/Support";
+// import CreateBanner from "./components/banner/CreateBanner";
+// import UpdateBanner from "./components/banner/UpdateBanner";
+// import DisplayBanner from "./components/section/DisplayBanner";
+// import Layout from './components/dashboard/Layout';
+// import { CheckboxProvider } from './context/CheckboxContext';
+// import { CheckboxArrProvider } from './context/CheckboxListContext'
+// function App({ userInfo }) {
+//   return (
+//     <CheckboxProvider>
+//     <CheckboxArrProvider>
+//     <Router>
+//       <Home>
+//         <Layout>
+//           <Switch>
+//             <Route path="/about-us" exact component={AboutUs} />
+//             <Route path="/banner/manage" exact component={BannerManage} />
+//             <Route path="/banner/create" exact component={CreateBanner} />
+//             <Route path="/banner/update/:code" exact component={UpdateBanner} />
+//             <Route path="/banner/update" exact component={UpdateBanner} />
+//             <Route path="/banner/delete" exact component={DisplayBanner} />
+//             <Route path="/contact" exact component={Contact} />
+//           </Switch>
+//         </Layout>
+//       </Home>
+//     </Router>
+//     </CheckboxArrProvider>
+//     </CheckboxProvider>
+//   );
+// }
+// export const Home = styled.div`
+//   display: flex;
+// `;
+
+// export default App;
