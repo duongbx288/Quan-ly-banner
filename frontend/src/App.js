97448d17940e4e6c00bd9eedf7f88695af215c07
Le Ngoc Kien <lengockien2000@gmail.com>
2022-04-25 09:27:04 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,31 +10,6 @@ import CreateBanner from "./components/banner/CreateBanner";
 import UpdateBanner from "./components/banner/UpdateBanner";
 import DisplayBanner from "./components/section/DisplayBanner";
 import Layout from './components/dashboard/Layout';
-<<<<<<< HEAD
-import SectionList from './components/section/SectionList';
-import SapoWeb from './dashboard/SapoWeb';
-
-function App({ userInfo }) {
-  return (
-    <Router>
-      <Home>
-        <Layout>
-          <Switch>
-            <Route path="/about-us" exact component={AboutUs} />
-            <Route path="/banner/manage" exact component={BannerManage} />
-            <Route path="/banner/create" exact component={CreateBanner} />
-            <Route path="/banner/update/:code" exact component={UpdateBanner} />
-            <Route path="/banner/update" exact component={UpdateBanner} />
-            <Route path="/banner/delete" exact component={DisplayBanner} />
-            <Route path="/contact" exact component={Contact} />
-            <Route path="/section" exact component={SectionList} />
-            <Route path="/section/SapoWeb" exact component={SapoWeb} />
-          </Switch>
-        </Layout>
-      </Home>
-    </Router>
-  );
-=======
 
 import AuthService from "./services/auth";
 import EventBus from "./common/EventBus";
@@ -157,7 +132,6 @@ class App extends Component {
       </div>
     );
   }
->>>>>>> 1f58ebbe42fe8b6a215f01f7a2b441c063f6598a
 }
 
 export default App;
